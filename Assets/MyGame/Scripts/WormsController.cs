fa23cc5df69cdaa945ffc4e44314d0305f500337
Lyra Cain <denis.cain@htl-salzburg.ac.at>
2020-02-06 03:14:47 +0100

    initial commit

---
+++ Assets/MyGame/Scripts/WormsController.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormsController : MonoBehaviour
+{
+    public Rigidbody rbAmmo;
+    public Rigidbody2D rb;
+
+    public Transform GroundCheck;
+    public LayerMask groundLayers;
+
+    public Animator animator;
+
+    public float jumpForce;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    public float moveSpeed;
+    public float AmmoForce;
+    public float MovementSmoothing;
+    Vector2 movement;
+    private bool isFacingRight;
+    public bool isGrounded;
+    private bool isJumping = false;
+
+    public KeyCode Jump;
+    public KeyCode Left;
+    public KeyCode Right;
+    public KeyCode Gun;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        animator.SetBool("isWalking", true);
+        float JumpVelocity = rb.velocity.y;
+        animator.SetFloat("JumpVel", JumpVelocity);
+
+        if (isJumping == true)
+        {
+            animator.SetBool("isJumping", true);
+        }
+        else
+        {
+            animator.SetBool("isJumping", false);
+        }
+
+        if (Input.GetKeyDown(Jump))
+        {
+            Move(0, true);
+            animator.SetTrigger("Jump");
+        }
+
+        if (Input.GetKey(Right))
+        {
+            //isFacingRight = true;
+            Move(10, false);
+        }
+
+        if (Input.GetKey(Left))
+        {
+            //isFacingRight = false;
+            Move(-10, false);
+        }
+
+        if (Input.GetKeyDown(Gun))
+        {
+            Shüt();
+        }
+    }
+
+    void Shüt()
+    {
+        Rigidbody clone;
+        int Direction;
+        if (isFacingRight == true)
+        {
+            Direction = 1;
+        }
+        else
+        {
+            Direction = -1;
+        }
+
+
+        Vector3 fix = new Vector3(0.5f, 0.5f, 0f);
+        clone = Instantiate(rbAmmo, transform.position + fix, transform.rotation);
+        clone.velocity = transform.TransformDirection(AmmoForce * Direction, 0f, 0f);
+
+        Vector3 keyr = new Vector3(AmmoForce * Direction, 0, 0);
+        rbAmmo.AddForce(keyr);
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, 0.1f, groundLayers);
+
+        if (isGrounded == true)
+        {
+            animator.SetTrigger("Land");
+            animator.SetBool("isGrounded", true);
+        }
+        else
+        {
+            animator.SetBool("isGrounded", false);
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(Jump))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+        }
+
+    }
+
+    public void HasLanded()
+    {
+        isJumping = false;
+    }
+
+    public void Move(float move, bool jump)
+    {
+        Vector2 refVel = Vector2.zero;
+
+        //if (isGrounded)
+        //{
+            animator.SetBool("isWalking", false);
+            Vector2 targetVelocity = new Vector2(move * moveSpeed, rb.velocity.y);
+            rb.velocity = Vector2.SmoothDamp(rb.velocity, targetVelocity, ref refVel, MovementSmoothing);
+
+            if (move > 0 && !isFacingRight || move < 0 && isFacingRight)
+            {
+                // flip player
+                Flip();
+            }
+        //}
+
+        if (isGrounded && jump)
+        {
+            isJumping = true;
+            rb.AddForce(new Vector2(0f, jumpForce * 25));
+        }
+
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector2 thisScale = transform.localScale;
+        thisScale.x *= -1;
+        transform.localScale = thisScale;
+    }
+
+}
