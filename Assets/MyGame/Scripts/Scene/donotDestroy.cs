7021ef217e31245656deedf8f19e634ccc8e8cf7
Lyra Cain <denis.cain@htl-salzburg.ac.at>
2020-03-01 18:20:50 +0100

    included temp audio files for testing

---
+++ Assets/MyGame/Scripts/Scene/donotDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class donotDestroy : MonoBehaviour
+{
+    public GameObject Ob01;
+    public GameObject Ob02;
+    public GameObject Ob03;
+    public GameObject Ob04;
+    public GameObject Ob05;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
